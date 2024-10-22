3b913c881c1db36b6544bd836b9cd8a91676e604
Velazquez24 <snakev635@gmail.com>
2024-10-22 20:16:29 -0300

    Update Productos.py

--- Productos.py
+++ Productos.py
@@ -20,37 +20,46 @@ while True:
 
     respuesta = int(input('Ingrese su opcion: '))
     if respuesta == 1:
-            an = int(input('ingrese el nombre de su producto'))
+            an = input('ingrese el nombre de su producto').strip().lower()
             ac = int(input('ingrese la cantidad de su producto'))
-            ap = int(input('ingrese el precio de su producto'))
+            ap = float(input('ingrese el precio de su producto'))
 
             cantidad.append(ac)
             productos.append(an)
             precio.append(ap)
+            
     elif respuesta == 2:
-            buscador = input('ingrese el nombre del producto para buscar')
+            buscador = input('ingrese el nombre del producto para buscar').strip().lower()
             posicion = productos.index(buscador)
             print('la cantidad del producto es: ', cantidad[posicion])
             print('el nombre del producto es: ', productos[posicion])
             print('el precio del producto es: ', precio[posicion])
-        
+
     elif respuesta == 3:
-            buscador = input('ingrese el producto que desea modificar: ')
+        buscador = input('Ingrese el producto que desea modificar: ').strip().lower()
+        try:
             posicion = productos.index(buscador)
-            an = int(input('ingrese el nombre de su producto: '))
-            ac = int(input('ingrese la cantidad de su producto: '))
-            ap = int(input('ingrese el precio de su producto: '))
+            an = input('Ingrese el nuevo nombre de su producto: ').strip().lower()
+            ac = int(input('Ingrese la nueva cantidad de su producto: '))
+            ap = float(input('Ingrese el nuevo precio de su producto: '))
             cantidad[posicion] = ac
             productos[posicion] = an
             precio[posicion] = ap
-        
+        except ValueError:
+            print('producto no encontrado')    
+                
     elif respuesta == 4:
-            print('La cantidad de productos es: ', cantidad)
-            print('el nombre del producto es ', productos)
-            print('el precio es ', precio)
-
+        buscador = input('Ingrese el nombre del producto que quiera eliminar: ').strip().lower()
+        try:
+                posicion = productos.index(buscador)
+                del productos[posicion]
+                del cantidad[posicion]
+                del precio[posicion]
+        except ValueError:
+              print('producto no encontrado')
     elif respuesta == 5:
            guardar_datos()
-           
+           print ('Datos Guardados correctamente')
     else:
         break
+    
